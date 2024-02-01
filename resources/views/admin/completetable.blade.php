81786de1faccc0b6040986297137ca1c3683f313
jamshidrajabov <jamshidamaliymatematika@gmail.com>
2024-02-01 12:00:56 +0500

    3-versiya

--- resources/views/admin/completetable.blade.php
+++ resources/views/admin/completetable.blade.php
@@ -11,7 +11,7 @@
   }
   table td,
   table th {
-    border: 1px solid #dadada;
+    border: 1px solid white;
     padding: 5px;
   }
   table thead th {
@@ -27,6 +27,14 @@
   }
   table thead th:nth-child(3){
     width: 30%;
+  }
+  .for_th
+  {
+    width: 150px;
+    background-color:rgb(230, 245, 245);
+    color: blue;
+    text-align: center;
+    border: 1px solid black;
   }
   </style>
   <?php
@@ -49,41 +57,41 @@
    <table  class="jadval">
     <tbody>
         <tr>
-            <th style="width: 150px">Murojatchi ismi</th>
+            <th class="for_th">Murojatchi ismi</th>
             <td>{{$appoints->name}}</td>
           </tr>
           <tr>
-            <th>Elektron pochta</th>
+            <th class="for_th">Elektron pochta</th>
             <td>{{$appoints->email}}</td>
           </tr>
           <tr>
-            <th>Telefon raqami</th>
+            <th class="for_th">Telefon raqami</th>
             <td>{{$appoints->phone}}</td>
           </tr>
           <tr>
-            <th>Sana</th>
+            <th class="for_th">Sana</th>
             <td>{{$appoints->date}}</td>
           </tr>
           <tr>
-            <th>Xabar</th>
+            <th class="for_th">Xabar</th>
             <td>{{$appoints->message}}</td>
           </tr>
           <tr>
-            <th>Shifokor</th>
+            <th class="for_th">Shifokor</th>
             <td>{{$appoints->doctor}}</td>
           </tr>
           <tr>
-            <th>Holati</th>
+            <th class="for_th">Holati</th>
             <td style="color:{{$status_color}}">{{$appoints->status}}</td>
           </tr>
           <tr>
-            <th>Amallar</th>
+            <th class="for_th">Amallar</th>
             <td>
-              <a  style="width:200px; " class="btn btn-success" style="color: green; font-size:17px;" href="{{url('approved',$appoints->id)}}">Tasdiqlash</a>
+              <a  style="width:100%; padding:20px" class="btn btn-success" style="color: green; font-size:17px;" href="{{url('approved',$appoints->id)}}">Tasdiqlash</a>
               <br><br>
-               <a  style="width:200px; " class="btn btn-danger" style="color: rgb(225, 27, 57); font-size:17px;" href="{{url('canceled',$appoints->id)}}">Rad qilish</a>
+               <a  style="width:100%; padding:20px" class="btn btn-danger" style="color: rgb(225, 27, 57); font-size:17px;" href="{{url('canceled',$appoints->id)}}">Rad qilish</a>
                <br><br>
-                <a  style="width:200px; " class="btn btn-primary" style="color: #f1f1f3; font-size:17px;" href="{{url('emailview',$appoints->id)}}">Javob berish</a>
+                <a  style="width:100%; padding:20px" class="btn btn-primary" style="color: #f1f1f3; font-size:17px;" href="{{url('emailview',$appoints->id)}}">Javob berish</a>
                
             </td>
           </tr>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -86,11 +86,11 @@
                         <x-slot name="content">
                             <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
+                                {{ __('Hisobni boshqarish') }}
                             </div>
 
                             <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                                {{ __('Profil') }}
                             </x-dropdown-link>
 
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -107,7 +107,7 @@
 
                                 <x-dropdown-link href="{{ route('logout') }}"
                                          @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
+                                    {{ __('Chiqish') }}
                                 </x-dropdown-link>
                             </form>
                         </x-slot>
@@ -153,7 +153,7 @@
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
                 <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
+                    {{ __('Profil') }}
                 </x-responsive-nav-link>
 
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -168,7 +168,7 @@
 
                     <x-responsive-nav-link href="{{ route('logout') }}"
                                    @click.prevent="$root.submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Chiqish') }}
                     </x-responsive-nav-link>
                 </form>
 
